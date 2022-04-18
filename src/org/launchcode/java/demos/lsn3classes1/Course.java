9611bd8ffb23329a01b52455535e5396f30cb718
unknown <dgray.stlouis@gmail.com>
2022-04-18 09:12:53 -0500

    chp4 exercise final version

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.HashMap;
+
+public class Course {
+    private HashMap<Integer,String> studentInfo;
+    private int courseId;
+    private String CourseName;
+}
